dc931cad74f22503e0777d72d41492b288562d1c
Amit Patil <amitpatil7790@gmail.com>
2025-07-22 13:49:18 +0530

    Commented Marketing app import

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import MarketingApp from "./components/MarketingApp";
+// import MarketingApp from "./components/MarketingApp";
 
 export default function App() {
   return (
     <div>
       <h1>Welcome to the Container App</h1>
       <p>This is the main application container.</p>
-      <MarketingApp />
+      {/* <MarketingApp /> */}
     </div>
   );
 }
